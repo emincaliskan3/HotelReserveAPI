734eff4595b95efdda193a88b1d3ae3b57f4c878
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-23 01:23:54 +0300

    Room-Staff-ApiController

---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddStaff(int id)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff(int id)
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff(int id)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff(int id)
+        {
+            return Ok();
+        }
+    }
+}
