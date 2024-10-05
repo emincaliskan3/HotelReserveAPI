b6eb762d4fe6150783794391ba2d144d631c5903
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-06 00:19:04 +0300

    login_dto

---
+++ Frontend/HotelProject.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.LoginDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(LoginUserDto loginUserDto)
+        {
+            return View();
+        }
+    }
+}
