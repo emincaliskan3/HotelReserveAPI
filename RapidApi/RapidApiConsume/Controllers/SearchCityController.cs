af0e4235d92cb788cfa04982504b19b2d3a31492
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-10 04:53:28 +0300

    rapidapi

---
+++ RapidApi/RapidApiConsume/Controllers/SearchCityController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchCityController : Controller
+    {
+        public async Task<IActionResult> Index(string cityName)
+        {
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name={cityName}"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "6efde1ea05msh480182dc91f0992p1131fejsn7853079fac06" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<CitySearchViewModel.Result>>(body);
+                    return View(values.ToList());
+                }
+            }
+            else
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name=Berlin"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "6efde1ea05msh480182dc91f0992p1131fejsn7853079fac06" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<CitySearchViewModel.Result>>(body);
+                    return View(values.ToList());
+                }
+            }
+        }
+    }
+}
