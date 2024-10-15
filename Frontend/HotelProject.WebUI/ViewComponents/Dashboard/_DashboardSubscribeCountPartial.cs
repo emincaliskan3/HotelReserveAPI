baf86325c46f07c423676a9ac25687d600c5cb5f
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-15 19:18:19 +0300

    ResultInstagramFollowersDto

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> Invoke()
         {
-            return View();
+            List<ResultInstagramFollowersDto> resultInstagramFollowersDtos = new List<ResultInstagramFollowersDto>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/leomessi"),
+                Headers =
+    {
+        { "x-rapidapi-key", "6efde1ea05msh480182dc91f0992p1131fejsn7853079fac06" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                resultInstagramFollowersDtos = JsonConvert.DeserializeObject<List<ResultInstagramFollowersDto>>(body);
+                return View(resultInstagramFollowersDtos);
+            }
+
         }
     }
 }
