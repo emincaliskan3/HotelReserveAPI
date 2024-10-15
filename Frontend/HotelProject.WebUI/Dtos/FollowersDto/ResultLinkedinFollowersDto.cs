c89fb4dbed71588d2595ea8e26741dde2e0af47c
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-15 23:32:32 +0300

    followersrapidapi

---
+++ Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultLinkedinFollowersDto.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelProject.WebUI.Dtos.FollowersDto
+{
+    public class ResultLinkedinFollowersDto
+    {
+        public Data data { get; set; }
+
+        public class Data
+        {
+            public int connection_count { get; set; }
+
+            public string location { get; set; }
+
+        }
+    }
+}
